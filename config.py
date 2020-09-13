ad65686eb330da5d6276c17bd9769c068ca81380
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-13 14:37:05 +0300

    added classes that would be used to define the general configuration process

---
+++ config.py
@@ -0,0 +1,15 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SOURCE_API_BASE_URL= 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
