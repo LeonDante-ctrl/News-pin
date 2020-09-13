24a2ccc30fa339adea4c3965f97450ef686511fd
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-13 14:47:33 +0300

    added a new object that would retreieve data from my request file

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 # Views
 @app.route('/')
@@ -8,4 +9,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    # Getting general sources
+    newproduct = get_sources('general')
+
+    print(newproduct)
+
+    title = 'Home - Welcome to The best News-Highlight Website Online'
+    return render_template('index.html', title = title, general = newproduct)
